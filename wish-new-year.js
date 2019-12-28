facd364992fff5dbc6d969fe9bf4c3409692bdc3
Lluís Martínez Suárez <luismartinezwebdev@gmail.com>
2019-12-28 14:27:18 +0100

    Linting with airbnb style

--- wish-new-year.js
+++ wish-new-year.js
@@ -1,34 +1,32 @@
-const NEW_YEAR = 2020
+const NEW_YEAR = 2020;
 
-const currentYear = new Date().getFullYear()
+const currentYear = new Date().getFullYear();
 
 const options = {
   specialEvent:
     currentYear === NEW_YEAR ? currentYear : `end of ${currentYear}`,
   goodThings: ['happy', 'joyful', 'wealthy', 'lovely', 'fun', 'crazy'],
   lovelyOnes: ['you all'],
-  excitementLevel: 3
-}
+  excitementLevel: 3,
+};
 
-const prependAnd = items => {
-  if (!Array.isArray(items) || items.length <= 1) return items
-  return items.map((item, index, arr) =>
-    index === arr.length - 1 ? `and ${item}` : item
-  )
-}
+const prependAnd = (items) => {
+  if (!Array.isArray(items) || items.length <= 1) return items;
+  return items.map((item, index, arr) => (index === arr.length - 1 ? `and ${item}` : item));
+};
 
 const makeBestWish = ({
   specialEvent = 'holiday',
   goodThings = ['good'],
   lovelyOnes = ['you'],
-  excitementLevel = 1
+  excitementLevel = 1,
 } = {}) => {
-  const _goodThings = prependAnd(goodThings)
-  const _lovelyOnes = prependAnd(lovelyOnes)
+  const parsedGoodThings = prependAnd(goodThings);
+  const parsedLovelyOnes = prependAnd(lovelyOnes);
 
-  return `I wish ${_lovelyOnes.join(', ')} a ${_goodThings.join(
-    ', '
-  )} ${specialEvent}${'!'.repeat(excitementLevel)}`
-}
+  return `I wish ${parsedLovelyOnes.join(', ')} a ${parsedGoodThings.join(
+    ', ',
+  )} ${specialEvent}${'!'.repeat(excitementLevel)}`;
+};
 
-console.log(makeBestWish(options))
+console.log(makeBestWish(options));
